2f908b85a70d47e1d0a72b384c5a9232a76daea6
JATIN879 <113619784+JATIN879@users.noreply.github.com>
2023-09-02 06:35:12 +0000

    variables

---
+++ .devcontainer/basic/variables.js
@@ -0,0 +1,18 @@
+console.log("jatin rathore");
+const  accountId = 144553;
+let accountEmail = "jatin.kumar12345w@gmail.com";
+var accountPassword = "12345";
+accountCity = "jaipur";
+let accountState;
+//accountId = 2;// not allowed
+accountEmail = "hc@hac.gmail.com"
+accountPassword = "7890"
+accountCity = "benglore"
+
+/* prefer not to use var 
+becoz of issue in block scope and functional scope
+*/
+
+
+//sare ke sare variables ek hi bar mai likh sakte hain by using:
+console.table([accountEmail,accountPassword,accountCity,accountState])
