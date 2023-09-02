c8aa419469b757d9afa673e2e17139b2f536ec58
JATIN879 <113619784+JATIN879@users.noreply.github.com>
2023-09-02 12:48:00 +0000

    Operations

---
+++ .devcontainer/basic/Operations.js
@@ -0,0 +1,36 @@
+//***********Operations*************//
+
+// let value =3;
+// let negvalue = -value;
+//console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**5);
+// console.log(2/3);
+// console.log(1%2);
+
+
+// let s1 = "jatin"
+// let s2 = "-SDE2"
+// let s3 = s1-s2;
+// console.log(s3);
+
+
+// console.log("1"+2+2);//problem
+// console.log(1+"2");
+// console.log("1"+"2");//problem
+// console.log(1+2+"2");
+
+let gameCounter =100;
+// gameCounter++; //101
+++gameCounter;  //101
+console.log(gameCounter)
+
+// link to study:
+//https://tc39.es/ecma262/multipage/abstract-operations.
+// html#sec-type-conversion
+
+
+
