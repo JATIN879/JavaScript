9c60ba5a5bd217ef4ac1dfec9e2afd4dcdb9556c
JATIN879 <113619784+JATIN879@users.noreply.github.com>
2023-09-02 07:02:17 +0000

    datatypes

---
+++ .devcontainer/basic/datatypes.js
@@ -0,0 +1,18 @@
+//"use strict"; //treat all js code as newer version
+
+//alert(2+2)//show err : we are using nodejs not any browser;;
+
+//PREMITIVE DATATYPES:
+//number => 2 to the power 53
+//bigInt =>  
+//string => " "
+//boolean =>  true/false
+//null =>  standalone value
+//undifined => variable declared but not assign any value:
+//symbol => uniques
+//object
+
+
+let isAdult = true;
+console.log(typeof undefined); //undefined
+console.log(typeof null);  //object
