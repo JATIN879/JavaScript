b9011a7ca2b5ac2c1172457cb06d39c84380aae8
JATIN879 <113619784+JATIN879@users.noreply.github.com>
2023-09-02 11:07:58 +0000

    ConversionsDT

---
+++ .devcontainer/basic/ConversionsOperations.js
@@ -0,0 +1,32 @@
+let score = "Jatin";
+console.log("type is :",typeof(score));
+
+
+//convert string into number:
+let  valueInNumber = Number(score);//string
+console.log(typeof valueInNumber);//number
+console.log(valueInNumber);//NaN
+
+
+
+//"33" -> 33
+//"33abc" -> NaN
+//true/false -> 1/0
+
+
+// in boolean conversion:
+let isLoggedIn = " " ;
+let bool = Boolean(isLoggedIn);
+console.log(typeof bool);
+console.log(bool)
+//"" -> false
+//"hitesh" -> true
+
+
+//in string conversions:
+let someNumber = 33;
+let StringNumber = String(someNumber);
+console.log(StringNumber);
+console.log(typeof StringNumbertringNumber);
+
+
