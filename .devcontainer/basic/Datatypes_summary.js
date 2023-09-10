51af0141d805cde1e0d6edba8303c116539fb92e
JATIN879 <113619784+JATIN879@users.noreply.github.com>
2023-09-10 06:24:06 +0000

    datatype-summary

---
+++ .devcontainer/basic/Datatypes_summary.js
@@ -0,0 +1,23 @@
+//Premitive DT:
+
+// 7 types: strings
+// const id = Symbol('123');
+// const anotherid = Symbol('123');
+// console.log(id === anotherid);
+//Arrays
+const heroes = ["shaktiman","naagraj","doge"];
+///objects
+let myobj={
+ name:"jatin",
+ age : 21,
+};
+//functions
+const myfunction=function(){
+    console.log("hello duniya walo");
+
+}
+myfunction();
+console.log(typeof myobj)
+console.log(typeof myfunction)
+
+//https://262.ecma-international.org/5.1/#sec-11.4.3
