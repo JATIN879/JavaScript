b39a81ea3f6b49a5ecb0ebd446b443ec5e982932
JATIN879 <113619784+JATIN879@users.noreply.github.com>
2023-09-10 06:48:58 +0000

    full DT summary

--- .devcontainer/basic/Datatypes_summary.js
+++ .devcontainer/basic/Datatypes_summary.js
@@ -21,3 +21,26 @@ console.log(typeof myobj)
 console.log(typeof myfunction)
 
 //https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// +++++memory+++++
+
+//stack(use in premitive), heap(use in non premitive);
+
+let name = "king"
+let anotherName = name;
+anotherName = "jatin"
+console.log(anotherName)
+console.log(name);
+
+let user1= {
+    name :"jatin kumar",
+    upi :"87923@paytm"
+
+}
+
+let user2 =  user1;
+user2.upi =  "Prashant@google.com"
+console.log(user1);
+console.log(user2);
