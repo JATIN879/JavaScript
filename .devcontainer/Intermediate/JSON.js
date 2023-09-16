642157b2eec603597046a1ee55812bb7b8a5eb0c
JATIN879 <113619784+JATIN879@users.noreply.github.com>
2023-09-16 07:33:15 +0000

    JSON AND API

---
+++ .devcontainer/Intermediate/JSON.js
@@ -0,0 +1,26 @@
+//Destructuring
+
+// const course ={
+//     coursename : "js in hindi",
+//     price : "999",
+//     courseInstructor : "Hitesh chaudhry"
+// }
+
+// const {price : p} = course
+// console.log(p);
+
+
+//JSON API brief Intoduction:
+// {
+//     name: "jatin",
+//     "coursename ": "js in hindi",
+//    "price" : "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
+
